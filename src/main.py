30190072891e5a86470bceeef12203a06fe6eb9b
Fide Omar Islas Duarte <fidev.id@outlook.com>
2022-08-16 22:43:27 -0500

    laod user labels

--- src/main.py
+++ src/main.py
@@ -1,13 +1,67 @@
-import psycopg2
+from __future__ import print_function
 
-try:
-    connection = psycopg2.connect(
-        host = 'localhost',
-        user = 'postgres',
-        password = 'p057gr35',
-        database = 'postgres'
-    )
-    print("conexión exitosa")
-except Exception as ex:
-    print(ex)
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def gmail_authenticate():
+    """Authenticate with gmail api"""
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = None
+    try:
+        # Build the Gmail API service
+        service = build('gmail', 'v1', credentials=creds)
+    except HttpError as error:
+        print(f'An error occurred building API service: {error}')
+    return service
+
+def main():
+    print('Logging in...')
+    service = gmail_authenticate()
+
+    if service is None:
+        print('Not API service available, ending execution...')
+        return
+    try:
+        print('Success login!')
+        # Call the Gmail API
+        results = service.users().labels().list(userId='me').execute()
+        labels = results.get('labels', [])
+
+        if not labels:
+            print('No labels found.')
+            return
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+    except HttpError as error:
+        print(f'An error occurred calling API: {error}')
+
+if __name__ == '__main__':
+    main()
 
